795319c89125fbe982bcfda6a0548d2457c930f9
Stream <1542763342@qq.com>
2025-02-25 22:20:42 +0800

    feat: make KUnion completely non-null

--- src/commonMain/kotlin/io/github/stream29/union/KUnion.kt
+++ src/commonMain/kotlin/io/github/stream29/union/KUnion.kt
@@ -8,5 +8,5 @@ import kotlin.jvm.JvmInline
 public value class KUnion
 <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any, T8 : Any, T9 : Any, T10 : Any, T11 : Any, T12 : Any, T13 : Any, T14 : Any, T15 : Any, T16 : Any, T17 : Any, T18 : Any, T19 : Any, T20 : Any, T21 : Any, T22 : Any>
 @PublishedApi internal constructor(
-    public val value: Any?
+    public val value: Any
 )
