1bb87e204701f8af54f9639f9a9e348212f93444
Stream <1542763342@qq.com>
2025-02-27 14:12:13 +0800

    feat: add contract for consumeN

--- src/commonMain/kotlin/io/github/stream29/union/Consume.kt
+++ src/commonMain/kotlin/io/github/stream29/union/Consume.kt
@@ -1,9 +1,18 @@
+@file:OptIn(ExperimentalContracts::class)
+
 package io.github.stream29.union
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 public inline fun <reified T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any, T8 : Any, T9 : Any, T10 : Any, T11 : Any, T12 : Any, T13 : Any, T14 : Any, T15 : Any, T16 : Any, T17 : Any, T18 : Any, T19 : Any, T20 : Any, T21 : Any, T22 : Any>
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume0(
     consumer: (T0) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T0)
         consumer(value)
     return this
@@ -13,6 +22,9 @@ public inline fun <T0 : Any, reified T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume1(
     consumer: (T1) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T1)
         consumer(value)
     return this
@@ -22,6 +34,9 @@ public inline fun <T0 : Any, T1 : Any, reified T2 : Any, T3 : Any, T4 : Any, T5 
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume2(
     consumer: (T2) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T2)
         consumer(value)
     return this
@@ -31,6 +46,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, reified T3 : Any, T4 : Any, T5 
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume3(
     consumer: (T3) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T3)
         consumer(value)
     return this
@@ -40,6 +58,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, reified T4 : Any, T5 
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume4(
     consumer: (T4) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T4)
         consumer(value)
     return this
@@ -49,6 +70,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, reified T5 
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume5(
     consumer: (T5) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T5)
         consumer(value)
     return this
@@ -58,6 +82,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, r
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume6(
     consumer: (T6) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T6)
         consumer(value)
     return this
@@ -67,6 +94,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume7(
     consumer: (T7) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T7)
         consumer(value)
     return this
@@ -76,6 +106,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume8(
     consumer: (T8) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T8)
         consumer(value)
     return this
@@ -85,6 +118,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume9(
     consumer: (T9) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T9)
         consumer(value)
     return this
@@ -94,6 +130,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume10(
     consumer: (T10) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T10)
         consumer(value)
     return this
@@ -103,6 +142,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume11(
     consumer: (T11) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T11)
         consumer(value)
     return this
@@ -112,6 +154,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume12(
     consumer: (T12) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T12)
         consumer(value)
     return this
@@ -121,6 +166,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume13(
     consumer: (T13) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T13)
         consumer(value)
     return this
@@ -130,6 +178,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume14(
     consumer: (T14) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T14)
         consumer(value)
     return this
@@ -139,6 +190,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume15(
     consumer: (T15) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T15)
         consumer(value)
     return this
@@ -148,6 +202,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume16(
     consumer: (T16) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T16)
         consumer(value)
     return this
@@ -157,6 +214,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume17(
     consumer: (T17) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T17)
         consumer(value)
     return this
@@ -166,6 +226,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume18(
     consumer: (T18) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T18)
         consumer(value)
     return this
@@ -175,6 +238,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume19(
     consumer: (T19) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T19)
         consumer(value)
     return this
@@ -184,6 +250,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume20(
     consumer: (T20) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T20)
         consumer(value)
     return this
@@ -193,6 +262,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume21(
     consumer: (T21) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T21)
         consumer(value)
     return this
@@ -202,6 +274,9 @@ public inline fun <T0 : Any, T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T
         Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>.consume22(
     consumer: (T22) -> Unit
 ): Union<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> {
+    contract {
+        callsInPlace(consumer, InvocationKind.AT_MOST_ONCE)
+    }
     if (value is T22)
         consumer(value)
     return this
